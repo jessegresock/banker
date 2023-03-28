52bd619a5cc0f50d07eae065e2aa2de673a4815d
Jesse Gresock <jesse.gresock@gmail.com>
2023-03-28 01:27:33 +0000

    cleaning up old commented code

--- app/banker/views.py
+++ app/banker/views.py
@@ -8,6 +8,7 @@ from . import banker
 from .. import app
 
 
+# Get Postgres database connection details
 pg_user = app.config.get('PG_USER')
 pg_password = app.config.get('PG_PASSWORD')
 pg_host = app.config.get('PG_HOST')
@@ -15,14 +16,6 @@ pg_database = app.config.get('PG_DATABASE')
 
 # Configure the database connection
 engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}/{pg_database}')
-#metadata = MetaData()
-#testbankdata = Table('testbankdata', metadata,
-#    Column('status', String),
-#    Column('date', Date),
-#    Column('description', String),
-#    Column('debit', String),
-#    Column('credit', String)
-#)
 
 
 @banker.route('/', methods=['GET'])
@@ -46,30 +39,12 @@ def upload():
         except Exception as e:
             return "Error inserting the data into table:" + e
 
-        #data = []
-        #with open(file.filename, 'r') as f:
-        #    csvreader = csv.reader(f)
-        #    # Skip header row
-        #    next(csvreader)
-
-        #    for line in csvreader:
-        #        status, _date, description, debit, credit = line
-        #        data.append({'status': status, 'date': _date, 'description': description, 'debit': debit, 'credit': credit})
-        
-        #try:
-        #    # Insert the data into the database
-        #    with engine.connect() as conn:
-        #        conn.execute(testbankdata.delete())
-        #        conn.execute(testbankdata.insert(), data)
-        #        conn.commit()
-        #except Exception as e:
-        #    print("Error inserting the data into table:", e)
-        
         # Return a success message to the user
         return redirect('/banker/?success=true')
     
     # Render the file upload form
     return render_template('banker/upload.html')
+
 
 @banker.route('/view', methods=['GET'])
 def view():
